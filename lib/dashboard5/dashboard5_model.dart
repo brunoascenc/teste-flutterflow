aaa7ee8895e12676481834aac52ea868b6737877
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-13 17:20:30 +0000

    Updating to latest FlutterFlow output.

--- lib/dashboard5/dashboard5_model.dart
+++ lib/dashboard5/dashboard5_model.dart
@@ -16,6 +16,15 @@ class Dashboard5Model extends FlutterFlowModel {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // State field(s) for TextField widget.
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  // State field(s) for TextField widget.
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
+  // State field(s) for TextField widget.
+  TextEditingController? textController3;
+  String? Function(BuildContext, String?)? textController3Validator;
 
   /// Initialization and disposal methods.
 
@@ -23,6 +32,9 @@ class Dashboard5Model extends FlutterFlowModel {
 
   void dispose() {
     unfocusNode.dispose();
+    textController1?.dispose();
+    textController2?.dispose();
+    textController3?.dispose();
   }
 
   /// Action blocks are added here.
